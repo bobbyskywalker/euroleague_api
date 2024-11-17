c976f46fd4bd0f5bcf378eb52113fb75beb5fee4
Olek <aleksander.garbacz@gmail.com>
2024-11-17 13:36:22 +0100

    config, dockerfile fixes

--- config/venv_setup.sh
+++ config/venv_setup.sh
@@ -4,6 +4,8 @@ python3 -m venv API_env
 
 source API_env/bin/activate
 
-pip3 install -r requirements.txt
+pip install --upgrade pip
+
+pip install -r requirements.txt
 
 echo "Virtual environment and dependencies setup complete"
